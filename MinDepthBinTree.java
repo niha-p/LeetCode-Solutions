246356c294f709207cab869e5e12fbd4cd9c9f67
niha-p <niharika.purbey@gmail.com>
2016-05-23 00:27:03 +0530

    cleaning

--- MinDepthBinTree.java
+++ MinDepthBinTree.java
@@ -4,13 +4,15 @@ https://leetcode.com/problems/minimum-depth-of-binary-tree/
 
 /*
  * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
  */
+ public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+ 
+/* SOLUTION */ 
 public class MinDepthBinTree {
     public int minDepth(TreeNode root) {
         if(root==null)
